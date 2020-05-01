cf5ff713728fa5560cae588d1a8333b3b7ee43a0
arun kumar <alekhya0801@gmail.com>
2020-05-01 17:37:36 +0530

    Refracted code with proper method names

--- src/test/java/com/moodAnalyser/MoodAnalyserTest.java
+++ src/test/java/com/moodAnalyser/MoodAnalyserTest.java
@@ -1,22 +1,23 @@
-import com.MoodAnalyser;
-import com.MoodAnalysisException;
+package com.moodAnalyser.moodAnalyserTest;
+
+import com.moodAnalyser.MoodAnalysisException;
+import com.moodAnalyser.MoodAnalyser;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenMessage_ifSad_thenReturnSad() throws MoodAnalysisException {
+    public void givenMessage_whenSad_shouldReturnSad() throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", mood);
     }
     @Test
-    public void givenMessage_ifNotSad_thenReturnHappy() throws MoodAnalysisException {
+    public void givenMessage_whenNotSad_shouldReturnHappy() throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", mood);
     }
-
     @Test
     public void givenNull_shouldThrowException() {
         MoodAnalyser moodAnalyser = new MoodAnalyser(null);

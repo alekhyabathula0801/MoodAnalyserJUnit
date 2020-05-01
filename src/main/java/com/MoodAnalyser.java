e7707cd8f17e61ce663c2d92f7e9d70c7a633351
arun kumar <alekhya0801@gmail.com>
2020-05-01 13:32:38 +0530

    Added feature to inform user if entered invalid mood

--- src/main/java/com/MoodAnalyser.java
+++ src/main/java/com/MoodAnalyser.java
@@ -7,9 +7,16 @@ public class MoodAnalyser {
         this.moodMessage = moodMessage;
     }
 
-    public String analyseMood() {
-        if(moodMessage.contains("Sad"))
-            return "SAD";
-        return "HAPPY";
+    public String analyseMood() throws MoodAnalysisException {
+        try {
+            if (moodMessage.length() == 0)
+                throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_EMPTY,"Please enter proper mood");
+            else if (moodMessage.contains("Sad"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.ENTERED_NULL,"Please enter proper mood");
+        }
     }
 }
--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -1,18 +1,38 @@
 import com.MoodAnalyser;
+import com.MoodAnalysisException;
 import org.junit.Assert;
 import org.junit.Test;
 
 public class MoodAnalyserTest {
     @Test
-    public void givenMessage_ifSad_thenReturnSad() {
+    public void givenMessage_ifSad_thenReturnSad() throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Sad Mood");
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("SAD", mood);
     }
     @Test
-    public void givenMessage_ifNotSad_thenReturnHappy() {
+    public void givenMessage_ifNotSad_thenReturnHappy() throws MoodAnalysisException {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
         String mood = moodAnalyser.analyseMood();
         Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void givenNull_shouldThrowException() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        try {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_NULL, e.type);
+        }
+    }
+    @Test
+    public void givenEmpty_shouldThrowException() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser("");
+        try {
+            moodAnalyser.analyseMood();
+        } catch (MoodAnalysisException e) {
+            Assert.assertEquals(MoodAnalysisException.ExceptionType.ENTERED_EMPTY, e.type);
+        }
     }
 }
